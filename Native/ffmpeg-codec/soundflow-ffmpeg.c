620c67f8601225d91157d90c9c804ad48c2b0a1e
Frooxius <tomas.mariancik@gmail.com>
2026-05-06 02:29:34 +0200

    chore#: I'm dummy and offset in wrong dir

--- Native/ffmpeg-codec/soundflow-ffmpeg.c
+++ Native/ffmpeg-codec/soundflow-ffmpeg.c
@@ -322,7 +322,7 @@ SF_FFMPEG_API SF_Result sf_decoder_read_pcm_frames(SF_Decoder* decoder, void* pF
                 }
 
                 if (*out_start_frame == -1)
-                    *out_start_frame = decoder->packet->pts + decoder->start_pts;
+                    *out_start_frame = decoder->packet->pts - decoder->start_pts;
 
                 if (avcodec_send_packet(decoder->codec_ctx, decoder->packet) < 0) {
                     av_packet_unref(decoder->packet);
