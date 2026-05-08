aaeabd3981163d02195ced37b686402983c46f0a
Frooxius <tomas.mariancik@gmail.com>
2026-05-08 05:04:17 +0200

    chore#: Flush before again?

--- Native/ffmpeg-codec/soundflow-ffmpeg.c
+++ Native/ffmpeg-codec/soundflow-ffmpeg.c
@@ -348,11 +348,6 @@ SF_FFMPEG_API SF_Result sf_decoder_seek_to_pcm_frame(SF_Decoder* decoder, int64_
     AVStream* stream = decoder->format_ctx->streams[decoder->stream_index];
     int64_t timestamp = av_rescale_q(frameIndex, (AVRational){1, stream->codecpar->sample_rate}, stream->time_base);
 
-    int ret = av_seek_frame(decoder->format_ctx, decoder->stream_index, timestamp, AVSEEK_FLAG_BACKWARD);
-    if (ret < 0) {
-        return SF_RESULT_DECODER_ERROR_SEEK_FAILED;
-    }
-
     // Flush buffers and seek
     avcodec_flush_buffers(decoder->codec_ctx);
     avformat_flush(decoder->format_ctx);
@@ -361,6 +356,11 @@ SF_FFMPEG_API SF_Result sf_decoder_seek_to_pcm_frame(SF_Decoder* decoder, int64_
     swr_close(decoder->swr_ctx);
     if (swr_init(decoder->swr_ctx) < 0) {
         return SF_RESULT_DECODER_ERROR_RESAMPLER_INIT_FAILED;
+    }
+
+    int ret = av_seek_frame(decoder->format_ctx, decoder->stream_index, timestamp, AVSEEK_FLAG_BACKWARD);
+    if (ret < 0) {
+        return SF_RESULT_DECODER_ERROR_SEEK_FAILED;
     }
 
     return SF_RESULT_SUCCESS;
